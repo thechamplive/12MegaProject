737d60b8ee26d15a9784024f95ee3fbcdae5eb9d
Sohaan <sohaanmetai@gmail.com>
2024-04-30 00:30:51 -0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Footer from './footer/Footer'
+import Header from './header/Header'
+import Container from './container/Container'
+import Logo from './Logo'
+import LogoutBtn from './header/LogoutBtn'
+import Button from './Button'
+import Input from './Input'
+import RTE from './RTE'
+import Signup from './Signup'
+import PostForm from './post-form/PostForm'
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
+import Select from './Select'
+import Login from './Login'
+
+export {
+    Footer,
+    Header,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    RTE,
+    Signup, 
+    Input,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Select,
+    Login
+}
